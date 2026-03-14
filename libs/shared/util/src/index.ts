71b7f99dcafbfc392edfa52c281daf99e7487c75
jovan-dyaz <jvaonam@me.com>
2026-03-14 00:47:31 -0600

    feat(shared): add voice note types, i18n translations, and language utility

--- libs/shared/util/src/index.ts
+++ libs/shared/util/src/index.ts
@@ -1,5 +1,6 @@
 export { formatRelativeTime } from './format-relative-time';
 export { generateId } from './generateId';
 export { getInitials } from './get-initials';
+export { getISOLanguage } from './get-iso-language';
 export { logger } from './logger';
 export { pickDefined } from './pick-defined';
