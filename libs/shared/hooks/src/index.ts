057ba6af5e056f821f13b11e179cea00fa102abc
jovan-dyaz <jvaonam@me.com>
2026-03-18 19:39:19 -0600

    feat(shared-hooks): add useMediaQuery hook

--- libs/shared/hooks/src/index.ts
+++ libs/shared/hooks/src/index.ts
@@ -2,5 +2,6 @@ export { useCollapsible } from './useCollapsible';
 export { useDebounce } from './useDebounce';
 export { useDebouncedCallback } from './useDebouncedCallback';
 export { useLatestRef } from './useLatestRef';
+export { useMediaQuery } from './useMediaQuery';
 export { useScrollDirection } from './useScrollDirection';
 export { useTypewriter } from './useTypewriter';
